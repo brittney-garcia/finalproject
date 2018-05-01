e6b28763d01e83708d450690027137707d673dd5
brittney-garcia <35813450+brittney-garcia@users.noreply.github.com>
2018-05-01 10:24:03 -0700

    Update sketch.js

--- p5-finalproject/sketch.js
+++ p5-finalproject/sketch.js
@@ -4,7 +4,7 @@ var player;
 //starting score
 var score = 20;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
   coins = new Group();
   for (var i = 0; i < 20; i++) {
 //cluster spacing
